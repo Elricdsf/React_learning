5f5e36b9e1383b257f752c47f22ea49d0299fab7
Elricdsf <daishunfeng21@gmail.com>
2021-03-02 15:35:11 +0800

    FaqSection draft

--- react-portofolio/src/pages/AboutUs.js
+++ react-portofolio/src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSectin from '../components/FaqSection';
 
 const AboutUs = ()=>{
     return(
         <>
             <AboutSection/>
             <ServicesSection/>
+            <FaqSectin/>
         </>
     )
 }
